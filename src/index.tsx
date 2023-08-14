470aeabf142462ac212d86620cf8ffcb505e0435
JacobDel <jacob.delabie@hotmail.com>
2023-08-14 10:24:11 +0200

    fix rerender issues
    Would rerender every render in parent components

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { PixelRatio } from "react-native";
 import ReactDOMServer from "react-dom/server";
 import Image from "react-native-remote-svg";
-import { default as AvatarReact, Piece as PieceReact } from "avataaars";
+import { default as AvatarReact, Piece as PieceReact } from "avataaars/src/index";
 
 export interface Props {
 	avatarStyle: string;
@@ -23,50 +23,46 @@ export interface Props {
 	pieceSize?: string;
 }
 
-export class Avatar extends React.Component<Props> {
-	render() {
-		const { size } = this.props;
-		return (
-			<Image
-				source={{
-					uri: `data:image/svg+xml;utf8,${ReactDOMServer.renderToString(
-						<AvatarReact
-							style={{
-								width: PixelRatio.getPixelSizeForLayoutSize(
-									size
-								),
-								height: PixelRatio.getPixelSizeForLayoutSize(
-									size
-								)
-							}}
-							{...this.props}
-						/>
-					)}`
-				}}
-				style={{
-					width: size,
-					height: size
-				}}
-			/>
-		);
-	}
-}
+export const Avatar = React.memo((props: Props) => {
+	const { size } = props;
+	return (
+		<Image
+			source={{
+				uri: `data:image/svg+xml;utf8,${ReactDOMServer.renderToString(
+					<AvatarReact
+						style={{
+							width: PixelRatio.getPixelSizeForLayoutSize(
+								size
+							),
+							height: PixelRatio.getPixelSizeForLayoutSize(
+								size
+							)
+						}}
+						{...props}
+					/>
+				)}`
+			}}
+			style={{
+				width: size,
+				height: size
+			}}
+		/>
+	);
+});
 
-export class Piece extends React.Component<Props> {
-	render() {
-		const { pieceSize } = this.props;
-		return (
-			<Image
-				source={{
-					uri: `data:image/svg+xml;utf8,${ReactDOMServer.renderToString(
-						<PieceReact {...this.props} />
-					)}`
-				}}
-				style={{
-					width: Number(pieceSize),
-					height: Number(pieceSize)
-				}}
-			/>
-		);
-	}
-}
+export const Piece = React.memo((props: Props) => {
+	const { pieceSize } = props;
+	return (
+		<Image
+			source={{
+				uri: `data:image/svg+xml;utf8,${ReactDOMServer.renderToString(
+					<PieceReact {...props} />
+				)}`
+			}}
+			style={{
+				width: Number(pieceSize),
+				height: Number(pieceSize)
+			}}
+		/>
+	);
+});
